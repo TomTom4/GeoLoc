0d49ee5e483bfbf3be0af658f345fcfba327b0c2
Labonneguigue <granie.guillaume@gmail.com>
2016-12-04 01:10:32 +0100

    OK

--- georouter/rapidxml-1.13/parserV1.cpp
+++ georouter/rapidxml-1.13/parserV1.cpp
@@ -9,8 +9,6 @@
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 
-#include "test.hpp"
-
 using namespace rapidxml;
 using namespace cv;
 using namespace std;
@@ -76,13 +74,20 @@ int GetDisplayY(double lat){
 
 class Node{
 
-	public:
+private:
+	string Name;
+	int user = 0;
 	int id;
 	float latitude;
 	float longitude;
 
+public:
 	void ToString(){
-		cout << "My node -- id : " << id << " longitude : " << longitude << " and latitude : " << latitude << "\n";
+		if (user){
+			cout << "Node called : " << Name << "with id : " << id << " longitude : " << longitude << " and latitude : " << latitude << "\n";
+		} else {
+			cout << "My node with id : " << id << " longitude : " << longitude << " and latitude : " << latitude << "\n";
+		}
 	}
 	Node(){
 		id = 0;
@@ -93,11 +98,33 @@ class Node{
 		longitude = lon;
 		latitude = lat;
 	}
+
+	void SetName(string NAME){
+		Name = NAME;
+		user = 1;
+	}
+
+	void Display(){
+		if (user){
+			circle(image, Point(GetDisplayX(longitude), GetDisplayY(latitude)),  5, Scalar(0, 20, 170, 255), 1, 8, 0);
+		}
+	}
+
+	float GetLatitude(){
+		return latitude;
+	}
+	float GetLongitude(){
+		return longitude;
+	}
+	int GetId(){
+		return id;
+	}
+
 };
 
 class Road{
 
-	public:
+public:
 	int id;
 	Node begin;
 	Node end;
@@ -114,11 +141,7 @@ class Road{
 		id = Road_Id;
 		begin = b;
 		end = e;
-	}
-
-	void Compute(){
-		a = (end.latitude - begin.latitude)/(end.longitude - begin.longitude);
-		b = begin.latitude - begin.longitude * a;
+		Compute();
 	}
 
 	double DistanceToCenter(double lon, double lat){
@@ -129,27 +152,35 @@ class Road{
 
 	void Display(){
 		if (Alpha){		// Alpha needs to be defined to get (x,y) coordinates
-			int y1 = GetDisplayY(begin.latitude);
-			int x1 = GetDisplayX(begin.longitude);
-			int y2 = GetDisplayY(end.latitude);
-			int x2 = GetDisplayX(end.longitude);
+			int y1 = GetDisplayY(begin.GetLatitude());
+			int x1 = GetDisplayX(begin.GetLongitude());
+			int y2 = GetDisplayY(end.GetLatitude());
+			int x2 = GetDisplayX(end.GetLongitude());
 			if (id == CurrentRoad){
-				line(image, Point(x1,y1), Point(x2, y2), Scalar( 255, 0, 0, 255),  3, 0 );
+				line(image, Point(x1,y1), Point(x2, y2), Scalar( 255, 0, 0, 255),  1, 0 );
 			} else {
-				line(image, Point(x1,y1), Point(x2, y2), Scalar( 110, 220, 100),  3, 0 );
+				line(image, Point(x1,y1), Point(x2, y2), Scalar( 110, 220, 100),  1, 0 );
 			}
 		}
+	}
+
+private:
+	void Compute(){
+		a = (end.GetLatitude() - begin.GetLatitude())/(end.GetLongitude() - begin.GetLongitude());
+		b = begin.GetLatitude() - begin.GetLongitude() * a;
 	}
 };
 
 class Building{
-	public:
+public:
 	int id;
 	string Name;
 	vector<Node> Building_Corner_Vec;
 
-	Building(int ID){
+	Building(int ID, string NAME, vector<Node> VEC){
 		id = ID;
+		Name = NAME;
+		Building_Corner_Vec = VEC;
 	}
 
 	void AddCorner(Node node){
@@ -162,13 +193,13 @@ class Building{
 			int VecSize = Building_Corner_Vec.size();
 			int NumberOfCoordinates = VecSize*2;
 			int XAndYs [NumberOfCoordinates];						// EVEN numbers are the Xs and ODD numbers are Ys
-			XAndYs[0] = GetDisplayX(Building_Corner_Vec[0].longitude);
-			XAndYs[1] = GetDisplayY(Building_Corner_Vec[0].latitude);
+			XAndYs[0] = GetDisplayX(Building_Corner_Vec[0].GetLongitude());
+			XAndYs[1] = GetDisplayY(Building_Corner_Vec[0].GetLatitude());
 			int index;
 			for(index = 1; index < VecSize ; index++){
-					XAndYs[index*2] = GetDisplayX(Building_Corner_Vec[index].longitude);
-					XAndYs[index*2+1] = GetDisplayY(Building_Corner_Vec[index].latitude);
-					line(image, Point(XAndYs[(index-1)*2],XAndYs[(index-1)*2+1]), Point(XAndYs[index*2], XAndYs[index*2+1]), Scalar( 0, 220, 100, 255),  1, 0 );
+				XAndYs[index*2] = GetDisplayX(Building_Corner_Vec[index].GetLongitude());
+				XAndYs[index*2+1] = GetDisplayY(Building_Corner_Vec[index].GetLatitude());
+				line(image, Point(XAndYs[(index-1)*2],XAndYs[(index-1)*2+1]), Point(XAndYs[index*2], XAndYs[index*2+1]), Scalar( 0, 220, 100, 255),  1, 0 );
 			}
 			line(image, Point(XAndYs[(index-1)*2],XAndYs[(index-1)*2+1]), Point(XAndYs[0], XAndYs[1]), Scalar( 0, 220, 100, 255),  1, 0 );
 		}
@@ -185,7 +216,7 @@ class Building{
 Node GetNodeById(int id, vector<Node> v){
 	Node null;
 	for(vector<Node>::iterator it = v.begin(); it != v.end(); ++it) {
-		if(it->id == id){
+		if(it->GetId() == id){
 			return *it;
 		}
 	}
@@ -195,6 +226,19 @@ Node GetNodeById(int id, vector<Node> v){
 //	Display every roads and the one we are on with a different color
 void DisplayAllRoads(vector<Road> v){
 	for(vector<Road>::iterator it = v.begin(); it != v.end(); ++it) {
+		it->Display();
+	}
+}
+
+//	Display every buildings
+void DisplayAllBuildings(vector<Building> v){
+	for(vector<Building>::iterator it = v.begin(); it != v.end(); ++it) {
+		it->Display();
+	}
+}
+
+void DisplayAllUserNodes(vector<Node> v){
+	for(vector<Node>::iterator it = v.begin(); it != v.end(); ++it) {
 		it->Display();
 	}
 }
@@ -239,30 +283,33 @@ int main(){
 		//opencvtest.o :
 		//	$(CC) $(CPPFLAGS) -c opencvtest.cpp
 
-		file<> xmlFile("heavy.xml");
+		file<> xmlFile("heavy.osm");
 		xml_document<> doc;
 		doc.parse<0>(xmlFile.data());
 		xml_node<> * root = doc.first_node();
 
 		vector<Node> Node_Vec;
+		vector<Road> Road_Vec;
+		vector<Building> Building_Vec;
+		vector<Node> User_Node;
 		Min_lon = 50;
 		Max_lon = 0;
 		Min_lat = 50;
 		Max_lat = 0;
 
-		vector<Road> Road_Vec;
-
 		int id = 0;
 		double lon = 0;
 		double lat = 0;
 		cout.precision(12);
-		int id_road = 0;
+		int id_way = 0;
 		xml_node<> * curr_node = root->first_node();
 
 		while(curr_node){
 
 			//Node
 			if(strcmp(curr_node->name(), "node")==0){
+				int user = 0;
+				string name;
 				id = 0;
 				lon = 0;
 				lat = 0;
@@ -277,12 +324,29 @@ int main(){
 					}
 					attr = attr->next_attribute();
 				}//while
+				xml_node<> * tag = curr_node->first_node();
+				while(tag){
+					if (strcmp(tag->name(), "tag") == 0){
+						if (strcmp(tag->first_attribute()->name(), "k") == 0){
+							if (strcmp(tag->first_attribute()->value(), "user") == 0){
+								user = 1;
+								name = string(tag->first_attribute()->next_attribute()->value());
+							}
+						}
+					}
+					tag = tag->next_sibling();
+				}
 
 				//Create node if we have all the needed values
 				if (id != 0 && lon != 0 && lat != 0){
 					Node node(id, lon, lat);
-					node.ToString();
-					Node_Vec.push_back(node);
+					if (user){
+						node.SetName(name);
+						User_Node.push_back(node);
+						node.ToString();
+					} else {
+						Node_Vec.push_back(node);
+					}
 					if (lon < Min_lon){
 						Min_lon = lon;
 					} else if (lon > Max_lon){
@@ -298,45 +362,68 @@ int main(){
 
 			//	Way
 			if(strcmp(curr_node->name(), "way")==0){
-				id_road = 0;
-				vector<Node> Vec_Road_Node;
+				id_way = 0;
+				int building = 0;
+				string Name = "" ;
+				vector<Node> Vec_Way_Node;
+
 				if (strcmp(curr_node->first_attribute()->name(), "id") == 0){
-					id_road = atoi(curr_node->first_attribute()->value());
+					id_way = atoi(curr_node->first_attribute()->value());
 					xml_node<> * curr_nd = curr_node->first_node();
 					while(curr_nd){
 						if (strcmp(curr_nd->name(), "nd") == 0){
 							if(strcmp(curr_nd->first_attribute()->name(), "ref") == 0){
-								int temp_id = atof(curr_nd->first_attribute()->value());
+								double temp_id = (double)atof(curr_nd->first_attribute()->value());
 								Node Retrieved_Node = GetNodeById(temp_id, Node_Vec);
-								if (Retrieved_Node.id != 0){
-									Vec_Road_Node.push_back(Retrieved_Node);
+								if (Retrieved_Node.GetId() != 0){
+									Vec_Way_Node.push_back(Retrieved_Node);
 								} else {
-									cout << "Pb GetNodeById()" << '\n';
+									//cout << "Pb GetNodeById()" << '\n';
 								}
 							} else {
-								cout << "Pb ref" << '\n';
+								//cout << "Pb ref" << '\n';
+							}
+						} else if (strcmp(curr_nd->name(), "tag") == 0){
+							if (strcmp(curr_nd->first_attribute()->name(), "k") == 0){
+								if (strcmp(curr_nd->first_attribute()->value(), "building") == 0){
+									building = 1;
+								} else if (strcmp(curr_nd->first_attribute()->name(), "name") == 0){
+									xml_attribute<> * attribute = curr_nd->first_attribute();
+									attribute = attribute->next_attribute();
+									Name = string(attribute->value());
+								}
 							}
 						}
 						curr_nd = curr_nd->next_sibling();
 					}
-					//Create Road if we have all the needed values
-					if (Vec_Road_Node.size() >= 2){
-						Road myRoad(id_road, Vec_Road_Node[0], Vec_Road_Node[Vec_Road_Node.size()-1]);
-						myRoad.Compute();
-						Road_Vec.push_back(myRoad);
+
+					// Create the appropriate environment element if we have all the needed values
+					if (Vec_Way_Node.size() >= 2){
+						// building
+						if (building == 1){
+							Building myBuilding(id_way, Name, Vec_Way_Node);
+							Building_Vec.push_back(myBuilding);
+						}
+						// Road
+						else if (building == 0){
+							Road myRoad(id_way, Vec_Way_Node[0], Vec_Way_Node[Vec_Way_Node.size()-1]);
+							Road_Vec.push_back(myRoad);
+						}
 					} else {
-						cout << "Only one point on this road" << '\n';
+						//cout << "Only one point for this element" << '\n';
 					}
 				} else {
-					cout << "Pb id" << '\n';
+					//cout << "Pb id" << '\n';
 				}
 			}
 			//	Next sibling
 			curr_node = curr_node->next_sibling();
+
 		}
 
 		std::cout << "Number of Nodes : " << Node_Vec.size() << '\n';
 		std::cout << "Number of Roads : " << Road_Vec.size() << '\n';
+		std::cout << "Number of User Nodes : " << User_Node.size() << '\n';
 
 		cout << "Which Road ? Point is 1.467821,43.570077 : \n";
 		WhichRoad(1.467821,43.570077, Road_Vec);
@@ -344,11 +431,13 @@ int main(){
 		SetAlpha();
 
 		DisplayAllRoads(Road_Vec);
-
+		DisplayAllBuildings(Building_Vec);
+		DisplayAllUserNodes(User_Node);
 		// Position
 		circle(image, Point(GetDisplayX(1.467821), GetDisplayY(43.570077)),  5, Scalar(0, 0, 255, 255), -1, 8, 0);
 		imshow("Image",image);
 		waitKey(0);
+
 	}
 	catch (const runtime_error& error)
 	{
