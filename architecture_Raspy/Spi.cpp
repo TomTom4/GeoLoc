97292a32fc19bba1e33635499db66a69c26725c9
TomTom4 <pouvreau@etud.insa-toulouse.fr>
2016-12-28 17:04:09 +0100

    adding singleton pattern to Spi

--- architecture_Raspy/Spi.cpp
+++ architecture_Raspy/Spi.cpp
@@ -1,4 +1,19 @@
 #include "Spi.h"
 
 using namespace std;
+
+Spi *Spi::s_instance = 0;
+
 	//Add your methodes over here 
+	Spi::Spi(){}
+	Spi* Spi::instance(){
+		if(!Spi::s_instance)
+			Spi::s_instance = new Spi();
+		return Spi::s_instance;	
+	}
+
+	void spiReadWriteData();
+	void spiClearString();
+	void spiAddData();
+	void extractData();
+	void spiMajCar();
--- architecture_Raspy/Spi.h
+++ architecture_Raspy/Spi.h
@@ -3,8 +3,21 @@
 
 class Spi{
 	public:
+		static Spi* instance(); 
+		void spiReadWriteData();
+		void spiClearString();
+		void spiAddData();
+		void extractData();
+		void spiMajCar();
 	private:
-
+		Spi();
+		static Spi *s_instance;
+		unsigned char m_length_string; 
+		unsigned char* m_string_spi;
+		unsigned char m_beacon_rasp_start;
+		unsigned char m_beacon_rasp_end;
+		unsigned char m_beacon_stm_start;
+		unsigned char m_beacon_stm_end;
 };
 
 #endif
