afcab539bce2bf4c28671d32b715ff7a8932fd3d
TomTom4 <pouvreau@etud.insa-toulouse.fr>
2017-01-13 18:08:48 +0100

    merging Navigation code with architecture

---
+++ architecture_Raspy/parserV1.hpp
@@ -0,0 +1,262 @@
+#ifndef PARSERV1_H
+#define PARSERV1_H
+
+#include <string>
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <tuple>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <math.h>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+
+#include "rapidxml_utils.hpp"
+#include "router.hpp"
+#include "rapidxml_utils.hpp"
+
+#define LOCAL_PI 3.1415926535897932385
+#define LARGEUR_FENETRE 700
+#define HAUTEUR_FENETRE 700
+#define DISTANCE_BETWEEN_ACQUISITIONS 5
+#define GPS_UNCERTAINTY 2.5
+
+using namespace std;
+
+
+
+
+
+
+
+/********************************/
+/*								*/
+/*		        NODE		    */
+/*								*/
+/********************************/
+
+class Node{
+
+private:
+    std::string Name;
+    int user = 0;
+    double id;
+    float latitude;
+    float longitude;
+    int NotRoutingNode = 0;
+
+public:
+    void ToString();
+    Node();
+
+    Node(double Node_Id, float lon, float lat);
+
+    void SetName(std::string NAME);
+
+    void Display(int close, cv::Mat imageToWriteOn);
+
+    void DisplayAsPathNode(int close, cv::Mat imageToWriteOn);
+
+    float GetLatitude();
+    float GetLongitude();
+    double GetId();
+    string GetName();
+    void NotARoutingNode();
+    int IsNotRoutingNode();
+    bool operator< (const Node &other);
+};
+
+
+
+
+
+
+
+/********************************/
+/*								*/
+/*			   ROAD			    */
+/*								*/
+/********************************/
+
+class Road{
+
+private:
+    double id;
+    Node begin;
+    Node end;
+    float a;
+    float b;
+    int width;
+    std::vector<Node> Road_Nodes;
+
+    void Compute_Coefs();
+
+public:
+    void ToString();
+
+    Road(double Road_Id, std::vector<Node> VEC);
+
+    Road(double Road_Id, Node b, Node e);
+
+    void SetEndNode(Node e);
+
+    void SetVecRoadNodes(std::vector<Node> VEC);
+
+    double DistanceToCenter(double lon, double lat);
+
+    void Display(int close, cv::Mat imageToWriteOn);
+
+    std::vector<Node> GetRoadNodes();
+    float GetA();
+    float GetB();
+    double GetId();
+    Node GetBegin();
+    Node GetEnd();
+};
+
+
+
+
+/********************************/
+/*								*/
+/*			BUILDING			*/
+/*								*/
+/********************************/
+
+class Building{
+
+private:
+    double id;
+    std::string Name;
+    std::vector<Node> Building_Corner_Vec;
+
+public:
+    Building(int ID, std::string NAME, std::vector<Node> VEC);
+
+    //	Enable the building to have more than 4 walls.
+    void Display(int close, cv::Mat imageToWriteOn);
+
+    std::string GetName();
+
+    double GetId();
+};
+
+/********************************/
+/*								*/
+/*			   MAP		        */
+/*								*/
+/********************************/
+
+class Map {
+
+private:
+    static double Min_lon;
+    static double Max_lon;
+    static double Min_lat;
+    static double Max_lat;
+    static double Delta_Lon;
+    static double Delta_Lat;
+
+    int BaseIndexToCount = 1000;
+
+    double CurrentPosition_Lon = 0;
+    double CurrentPosition_Lat = 0;
+    double PreviousPosition_Lon = 0;
+    double PreviousPosition_Lat = 0;
+    double DestinationPosition_Lon = 0;
+    double DestinationPosition_Lat = 0;
+
+    std::vector<Node *> Node_Vec;
+    std::vector<Road *> Road_Vec;
+    std::vector<Building *> Building_Vec;
+    std::vector<Node *> User_Node;
+
+    std::vector<Node *> PathToDestination;
+    int CurrentIntermediateDestinationNode;
+    int PathSet = 0;
+
+    int IntermediateDestinationReached = 0;
+    int FinalDestinationReached = 0;
+
+public:
+    static cv::Mat image;
+    static cv::Mat imageClose;
+    static double Alpha;
+    static double Beta;
+    static int CurrentRoad;
+
+    static double ToRadians(double degrees);
+
+    static double DirectDistance(double lat1, double lng1, double lat2, double lng2);
+
+    static int GetDisplayX(double lon);
+
+    static int GetDisplayY(double lat);
+
+    static int GetCloseDisplayX(double lon);
+
+    static int GetCloseDisplayY(double lat);
+
+    Node * GetClosestNode();
+
+    //	Display every roads and the one we are on with a different color
+    void DisplayAllRoads(std::vector<Road *> v, int close, cv::Mat imageToWriteOn);
+
+    //	Display every buildings
+    void DisplayAllBuildings(std::vector<Building *> v, int close, cv::Mat imageToWriteOn);
+
+    void DisplayAllUserNodes(std::vector<Node *> v, int close, cv::Mat imageToWriteOn);
+
+    void DisplayPath(int close);
+
+    void WhichRoad(double lon, double lat);
+
+    // Set the alpha factor that convert longitude/latitude in pixels
+    static void SetAlpha();
+
+    static void SetBeta();
+
+    float ComputeCoefA(Node begin, Node end);
+
+    void BuildAllRoads(int id_road, std::vector<Node> VEC);
+
+    Node * GetUserNodeById(double id);
+
+    // Returns the Node with the specified ID
+    Node * GetNodeById(double id);
+
+    Road * GetRoadById(double id);
+
+    vector<tuple<string, double>> GetTupleOfDestinations();
+
+    Map(char * OsmFilePath);
+
+    double WhichRoadWithLatLon();
+
+    int CreateAll(int close);
+
+    void Display(int close);
+
+    int DisplayImage(int close);
+
+    int SetPosition(double lon, double lat);
+
+    int DisplayMyPosition(int close);
+
+    //int DisplayCloseToLocation(cv::Mat imageToWriteOn);
+
+    float GetCorrectiveHeading(float DrivenDistance);
+
+    void SetDestination(double idDestNode);
+
+    void SetPath(std::vector<char *> path);
+
+};
+
+
+
+#endif
