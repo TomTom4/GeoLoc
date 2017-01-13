518c97cb9102123f1cd167adf93c50354bc422de
TomTom4 <pouvreau@etud.insa-toulouse.fr>
2017-01-13 18:00:50 +0100

    adding all Navigation code to architecture

--- architecture_Raspy/Geocar.cpp
+++ architecture_Raspy/Geocar.cpp
@@ -27,16 +27,89 @@ void testSpi(Mediator *mediator, Spi *spi){
 	mediator->addPwmMotorBack(0);
 	spi->majCar();
 }
-	
+
+void testNavigation(){
+
+	//rapidxml::file<> xmlFile("heavy.osm");
+	Map MyMap("heavy.osm");
+
+	std::vector<tuple<string, double>> MyVectorOfUserDestination = MyMap.GetTupleOfDestinations();
+
+	double ExamplePointLat = 43.570358;
+	double ExamplePointLon =  1.466047;
+
+	//double ExampleDestLat = 43.5708618;
+	//double ExampleDestLon = 1.4670463;
+	MyMap.SetPosition(ExamplePointLon,ExamplePointLat);
+	MyMap.SetDestination(-1858);
+	//MyMap.WhichRoadWithLatLon();
+
+	MyMap.Display(0);
+	MyMap.Display(1);
+
+	MyMap.SetPosition(1.466078, 43.570376);
+	std::cout << "GetCorrectiveHeading(3) : " << MyMap.GetCorrectiveHeading(3.0) << '\n';
+
+	MyMap.Display(1);
+
+	MyMap.SetPosition(1.466117,43.570386);
+	std::cout << "GetCorrectiveHeading(3) : " << MyMap.GetCorrectiveHeading(3.0) << '\n';
+
+	MyMap.Display(1);
+
+/*
+	MyMap.CreateAll(0, MyMap.image);
+	MyMap.DisplayMyPosition();
+	MyMap.DisplayImage(0);
+
+	MyMap.CreateAll(1, MyMap.imageClose);
+	MyMap.DisplayCloseMyPosition();
+	MyMap.DisplayImage(1);
+*/
+/*
+	// new
+
+	double CurrentClosestNode = MyMap.GetClosestNode()->GetId();
+
+	// Zepeng Code
+
+	Router MyRouter;
+
+	std::vector<char*> ipath;
+
+	//char pointA[7] = "-1630";
+	char pointA [10];
+	// Need to remove the decimals
+	sprintf (pointA, "%.0f", CurrentClosestNode);
+	std::cout << "Current closest node : " << pointA << '\n';
+	char pointB[7] = "-1858";
+
+	std::cout << "From : " << pointA << " ... to : " << pointB << '\n';
+	ipath = MyRouter.getpath(pointA,pointB);
+	int len = ipath.size();
+	std::cout << "ipath length : " << len << '\n';
+
+	// End Zepeng Code //
+
+	MyMap.SetPath(ipath);
+	std::cout << "The cap to follow : " << MyMap.CapAlgorithm() << '\n';
+	MyMap.DisplayPath(0, MyMap.image);
+	MyMap.DisplayImage(0);
+*/
+	return 1;
+}
+
 
 int main(){
 	Controler *controler;
 	Mediator *mediator;
 	Spi *spi;
+	Navigation *navigator;
 	
 	mediator = Mediator::instance();
 	controler = Controler::instance();
 	spi = Spi::instance();
+	navigator = Navigation::instance();
 	spi->startThread();
 
 	//temporary :only to test of spi
--- architecture_Raspy/Geocar.h
+++ architecture_Raspy/Geocar.h
@@ -2,9 +2,14 @@
 #define DEF_GEOCAR
 
 #include <string>
+#include <stdio.h>// c'est pas la même que iostream?
+#include <iostream>
+#include <tuple>
+
 #include "Controler.h"
 #include "Spi.h"
 #include "Mediator.h"
+#include "parserV1.hpp"
 
 class GeoCar{
 
