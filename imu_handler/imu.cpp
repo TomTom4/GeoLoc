8f983bb19552b3a3c8645bfa99382e60d8ce29c2
TomTom4 <pouvreau@etud.insa-toulouse.fr>
2017-01-18 17:32:49 +0100

    allllezzzzzz

---
+++ imu_handler/imu.cpp
@@ -0,0 +1,16 @@
+#ifdef __APPLE__
+   #include <Python/Python.h>
+#else
+    #include <Python.h>
+#endif
+#include <iostream>
+#include <string>
+
+
+using namespace std;
+
+int main(void){
+
+
+	return 0;
+}
