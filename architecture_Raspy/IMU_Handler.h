45f6f631fbc1df39d6901789fb132e9b8998d2ca
TomTom4 <pouvreau@etud.insa-toulouse.fr>
2017-01-20 15:08:55 +0100

    defining functions signatures in imu_handler

--- architecture_Raspy/IMU_Handler.h
+++ architecture_Raspy/IMU_Handler.h
@@ -2,11 +2,25 @@
 #define DEF_IMU_HANDLER
 
 #include <thread>
-#include <pthread.h>
+#include <pthread.h>	
+#include <string>
 
 #include "Model.h"
 #include "const.hpp"
 #include "Mediator.h"
+
+#include <iostream>
+#include <string>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <strings.h>
+#include <unistd.h>
+
+#define PORT 5555
+#define HOST NULL
+
+
 
 using namespace std;
 
@@ -16,7 +30,10 @@ public:
 	//** SINGLETON
 	static Imu* instance();
 	//** METHODS
-	void readHeading(void);
+	void readHeading();
+	int socketInit();
+	string readMessage(int sockfd);
+	void ParseMessage( string message);
 	//** THREAD
 	void* thImu(void);
 	static void* thImuHelper(void* context);
@@ -26,9 +43,9 @@ private:
 	Imu();
 	Mediator *m_mediator;
 	static Imu *s_instance;
+	string m_message;
 
 	//** ATTRIBUTES
-
 
 	//** ERROR
 	int result_code;
