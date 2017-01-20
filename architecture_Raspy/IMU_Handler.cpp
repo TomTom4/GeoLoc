7d8d3982477bba064fe1677fb026bfe5901b04bc
JauriG <gofre@etud.insa-toulouse.fr>
2017-01-20 14:34:22 +0100

    Ajout du lecteur d'imu
    bla

--- architecture_Raspy/IMU_Handler.cpp
+++ architecture_Raspy/IMU_Handler.cpp
@@ -50,3 +50,33 @@ IMU* Imu::instance()
 	{
 	  return((Imu*)context)->Imu::thImu();
 	}
+
+	void Imu::readHeading(void)
+	{
+		int portNumber, sockFileDescriptor, newSockFileDescriptor, clientAddressLength, RWcharNumber;
+		struct sockaddr_in server_addr, client_addr ;
+		char buffer[8192];
+
+		sockFileDescriptor = socket( AF_INET, SOCK_DGRAM, 0);
+		if(sockFileDescriptor  == -1)
+			cout << "You don't freaking work biatch !! "<<endl ;
+
+		bzero((char *) &server_addr, sizeof(server_addr));// here it set server_addr in memory with zero values
+
+
+		server_addr.sin_family = AF_INET;// should alway be that value
+		server_addr.sin_port = htons(PORT);// very important to convert using htons
+		server_addr.sin_addr.s_addr = INADDR_ANY;// this is a UDP Server, doesn't need to speak with anyone
+
+		if(bind(sockFileDescriptor, (struct sockaddr *) &server_addr, sizeof(server_addr)) < 0)
+			cout << "it doesn't bind you freaking basterd!!!!"<< endl;
+
+		while(1){
+			bzero(buffer,8192);
+			RWcharNumber = read(sockFileDescriptor, buffer, 8191);
+			if( RWcharNumber < 0 )
+				cout << "we are in PLS...." << endl;
+			else
+				cout << "this is the fucking message :" << buffer << endl ;
+		}
+	}
